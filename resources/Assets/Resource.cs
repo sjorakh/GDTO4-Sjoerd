ba13428e965209861ac33912b665ee600749c530
Sjorakh <355792@fhict.fontys.nl>
2018-03-08 14:22:28 +0100

    resources update
    het aanmaken van de resources cs file en de resource listener voor de ui interface

---
+++ resources/Assets/Resource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour {
+	public int StartingAmount;
+	public int CurrentOwned;
+
+	public bool AmountChanged;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void AddAmount(int x){
+		CurrentOwned += x;
+		AmountChanged = true;
+	}
+	public void RemoveAmount(int x){
+		CurrentOwned -= x;
+		AmountChanged = true;
+	}	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ resources/Assets/Resourcelistener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resourcelistener : Resource {
+	public Text display;
+	// Use this for initialization
+	void Start () {
+		CurrentOwned = StartingAmount;
+		display.text = StartingAmount.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (AmountChanged) {
+			display.text = CurrentOwned.ToString();
+			AmountChanged = false;
+		}
+	}
+}
